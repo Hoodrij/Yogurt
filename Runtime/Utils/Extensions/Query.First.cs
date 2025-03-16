b3c619045363166ebc786500ab1c41c80461a56d
Slava <Hoodrij@gmail.com>
2025-03-16 19:25:02 +0200

    + Query.First

--- Runtime/Utils/Extensions/Query.First.cs
+++ Runtime/Utils/Extensions/Query.First.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace Yogurt
 {
     public static partial class QueryEx
@@ -10,12 +12,34 @@ namespace Yogurt
                 : default;
         }
         
+        public static Entity First(this QueryOfEntity query, Predicate<Entity> predicate)
+        {
+            EntitiesEnumerator enumerator = query.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                if (predicate(enumerator.Current))
+                    return enumerator.Current;
+            }
+            return default;
+        }
+        
         public static TAspect First<TAspect>(this QueryOfAspect<TAspect> query) where TAspect : struct, IAspect
         {
             AspectsEnumerator<TAspect> enumerator = query.GetEnumerator();
             return enumerator.MoveNext() 
                 ? enumerator.Current 
                 : default;
+        }
+        
+        public static TAspect First<TAspect>(this QueryOfAspect<TAspect> query, Predicate<TAspect> predicate) where TAspect : struct, IAspect
+        {
+            AspectsEnumerator<TAspect> enumerator = query.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                if (predicate(enumerator.Current))
+                    return enumerator.Current;
+            }
+            return default;
         }
     }
 }
