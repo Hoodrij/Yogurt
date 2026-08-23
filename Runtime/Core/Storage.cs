aba60d7ddb87a27466e683c2b4545f67e6a916a6
Slava Korzun <Hoodrij@gmail.com>
2026-08-23 23:32:32 +0300

    Storage from flat array to paged array, so it consume much less memory at rare components

--- Runtime/Core/Storage.cs
+++ Runtime/Core/Storage.cs
@@ -42,50 +42,78 @@ namespace Yogurt
         public static Storage Of(ComponentID componentId) => all[componentId];
     }
 
+    
     internal class Storage<T> : Storage where T : IComponent
     {
+        internal const int PageSize = 1 << PageShift; // 4,096 entities per page
+        private const int PageShift = 12;
+        private const int PageMask = PageSize - 1;
+
         public static Storage<T> Instance => (Storage<T>)Of(ComponentID<T>.Value);
 
-        private T[] components = new T[Consts.INITIAL_ENTITIES_COUNT];
-        public override IComponent GetBoxed(Entity entity) => components[entity];
+        private T[][] pages = new T[1][];
+
+        public override IComponent GetBoxed(Entity entity)
+        {
+            int id = entity;
+            return pages[id >> PageShift][id & PageMask];
+        }
 
         public override void ClearEntity(Entity entity)
         {
-            if (entity < components.Length)
+            int id = entity;
+            int pageIndex = id >> PageShift;
+            if ((uint)pageIndex >= (uint)pages.Length)
+                return;
+
+            T[] page = pages[pageIndex];
+            if (page != null)
             {
-                components[entity] = default;
+                page[id & PageMask] = default;
             }
         }
 
         protected override void Reset()
         {
             Groups.Clear();
-            Array.Clear(components, 0, components.Length);
+
+            foreach (T[] page in pages)
+            {
+                if (page != null)
+                {
+                    Array.Clear(page, 0, page.Length);
+                }
+            }
         }
 
         public void Set(T component, Entity entity)
         {
-            AssureSize();
-            components[entity] = component;
-            return;
-
-            void AssureSize()
+            int id = entity;
+            int pageIndex = id >> PageShift;
+            if ((uint)pageIndex >= (uint)pages.Length)
             {
-                if (entity < components.Length)
-                    return;
-                int newSize = components.Length;
-                while (newSize <= entity)
-                {
-                    newSize *= 2;
-                }
-
-                Array.Resize(ref components, newSize);
+                EnsurePageTable(pageIndex + 1);
             }
+
+            T[] page = pages[pageIndex] ??= new T[PageSize];
+            page[id & PageMask] = component;
         }
 
         public ref T Get(Entity entity)
         {
-            return ref components[entity];
+            int id = entity;
+            return ref pages[id >> PageShift][id & PageMask];
+        }
+
+        private void EnsurePageTable(int requiredLength)
+        {
+            int newLength = pages.Length;
+            while (newLength < requiredLength)
+            {
+                newLength <<= 1;
+            }
+
+            Array.Resize(ref pages, newLength);
         }
     }
 }
